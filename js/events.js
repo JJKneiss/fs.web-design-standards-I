2cc121d453a0e03c1688a9316850979586ae7560
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2020-09-23 21:06:04 -0400

    Catch Up

--- js/events.js
+++ js/events.js
@@ -1,4 +1,10 @@
-/*
- * events.js
- * 
-*/
+class buttonEvents{
+    previousImage(event){
+        event.preventDefault();
+        console.log("hello");   
+    }
+}
+const backButton = document.getElementById("leftButton")
+if(backButton){
+    backButton.addEventListener("click", buttonEvents.previousImage);
+}
